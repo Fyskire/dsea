8953c6f2488e2b152f13f76bce4fde0d08a5a133
Fyskire <Yreinhar@students.uni-mainz.de>
2015-12-15 21:45:18 +0100

    Angelegt

---
+++ src/MyQueue.java
@@ -0,0 +1,26 @@
+
+public class MyQueue<E> {
+
+	public boolean push(E e){
+		return false;
+	}
+	
+	public E pop(){
+		E e = null;
+		return e;
+	}
+	
+	public int size(){
+		return 0;
+	}
+	
+	public void print(){
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

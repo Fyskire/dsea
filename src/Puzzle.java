39c9a46211b922535da7745f3fba05b5cc891e47
Fyskire <Yreinhar@students.uni-mainz.de>
2015-12-16 20:40:25 +0100

    2b)

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,10 +1,38 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class Puzzle {
 	
 	private int [][] arr;
-	private int groeszeX;
-	private int groeszeY;
+	private int groeszeX=3;
+	private int groeszeY=3;
 
+	
+	/**
+	 * Konstruktor
+	 * 2b) 
+	 * @param filepath
+	 */
+	public Puzzle(String filepath){
+		try{
+			Scanner s = new Scanner(new File(filepath));
+
+			String save = "";
+			arr = new int[groeszeY][groeszeX];
+			
+			//Baut immer erst Zeile auf, bevor die Spalte wechselt
+			while(s.hasNext()){
+				for(int i = 0; i<groeszeY; i++){
+					for(int j=0; j<groeszeX; j++){
+						arr[i][j] = Integer.parseInt(s.next());
+					}
+				}
+			}
+		} catch(Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+	
 	/**
 	 * Setzt die Belegung wie auf dem Blatt
 	 */
@@ -23,24 +51,27 @@ public class Puzzle {
 		arr[2][2]=8;
 	}
 	
-	private String print(){
+	/*
+	 * 2a) gibt Matrix als String zurück
+	 */
+	public String print(){
 		String erg = "";
-		
 		for(int i = 0; i<groeszeY; i++){
 			for(int j=0; j<groeszeX; j++){
 				erg += arr[i][j];
 			}
-		}
-		
+		}	
 		return erg;
 	}
+	
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Puzzle p = new Puzzle();
-		p.setDefault();
-		System.out.println(p.print());
+		Puzzle p = new Puzzle("C:\\Users\\Pussynator\\workspace\\DSEA\\src\\puzzle.txt");
+		//p.setDefault();
+		System.out.println("Inhalt der Matrix als String: " + p.print());
 	}
 
 }

4290761ed83222c05b6e470b236720f863a4dd96
jcasc <jcascitt@students.uni-mainz.de>
2015-12-16 18:14:44 +0100

    1a) text

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -57,6 +57,32 @@ public class MyQueue<E> {
         q.push(10);
         q.print();
 
+        /* 1a)
+         * Angenommen unsere Queue hat eine Kapazität von 50. Dann ist die anfängliche Situation wie folgt:
+         * head == 0
+         * tail == 49
+         * size == 0
+         * capacity == 50
+         *
+         * Beim einfügen von n Elementen wird n mal das neue Element auf der Position direkt hinter tail eingefügt,
+         * wobei zu beachten ist dass position 0 als Nachfolger der Position 49 behandelt wird.
+         * Das erste Element wird also in Position 0 geschrieben, das zweite in 1 etc.
+         * Bei jedem Einfügen werden tail und size inkrementiert.
+         * Ist n>50, wird nach 50 eingefügten Elementen erkannt, dass die size==capacity ist
+         * und direkt false zurückgegeben, anstatt ein weiteres Element einzufügen.
+         *
+         * Beim anschließenden Herausnehmen eines Elementes wird das Element auf der head position, also position 0,
+         * zwischengespeichert, die position 0 im Array auf null gesetzt, die size um 1 verringert,
+         * und head um 1 erhöht.
+         *
+         * Wird nun wieder ein Element hinzugefügt wird wieder auf die Position direkt hinter tail
+         * das neue Element geschrieben und tail und size um eins erhöht.
+         *
+         * Bei jedem In- und Dekrementiervorgang von head und tail ist zu beachten,
+         * dass die möglichen Positionen des Arrays wie ein Restklassenring über der Kapazität behandelt werden.
+         * Der Vorgänger von 0 ist also (hier) 49, und der Nachfolger von (hier) 49 ist 0.
+         */
+
         q = new MyQueue<Integer>(10);
         for (int i=0;i<15;i++) System.out.println(""+i+q.push(i));
         System.out.println(""+q.pop()+q.pop());

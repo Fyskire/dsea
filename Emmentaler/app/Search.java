9696a7940ca34e605f82bcd11a0e857124622ebe
jcasc <jcascitt@students.uni-mainz.de>
2016-01-07 23:35:57 +0100

    KÄSE

--- Emmentaler/app/Search.java
+++ Emmentaler/app/Search.java
@@ -2,15 +2,79 @@ package app;
 
 // java default stuff
 import java.util.Vector;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Deque;
+import java.util.Collection;
+import app.Tuple;
 
-class Search{
+class Search {
+
+    private static Set<Tuple> checked;
+    private static Deque<Vector<Tuple>> q;
+
+    private static void check(boolean [][][] cheese, Tuple currentNode, Vector<Tuple> path) {
+        Tuple newNode;
+        newNode = new Tuple(currentNode.zero-1, currentNode.one, currentNode.two);
+        if (legalNode(cheese, newNode)) remember(newNode, path);
+        newNode = new Tuple(currentNode.zero+1, currentNode.one, currentNode.two);
+        if (legalNode(cheese, newNode)) remember(newNode, path);
+        newNode = new Tuple(currentNode.zero, currentNode.one-1, currentNode.two);
+        if (legalNode(cheese, newNode)) remember(newNode, path);
+        newNode = new Tuple(currentNode.zero, currentNode.one+1, currentNode.two);
+        if (legalNode(cheese, newNode)) remember(newNode, path);
+        newNode = new Tuple(currentNode.zero, currentNode.one, currentNode.two-1);
+        if (legalNode(cheese, newNode)) remember(newNode, path);
+        newNode = new Tuple(currentNode.zero, currentNode.one, currentNode.two+1);
+        if (legalNode(cheese, newNode)) remember(newNode, path);
+    }
+
+    private static boolean legalNode(boolean [][][] cheese, Tuple node) {
+        return (node.zero >= 0 && node.zero < cheese.length &&
+            node.one >= 0 && node.one < cheese[0].length &&
+            node.two >= 0 && node.two < cheese[0][0].length &&
+            !cheese[node.zero][node.one][node.two] &&
+            !checked.contains(node));
+    }
+
+    static private void remember(Tuple node, Vector<Tuple> path) {
+        Vector<Tuple> newPath = new Vector<Tuple>(path);
+        newPath.add(node);
+        q.add(newPath);
+        checked.add(node);
+    }
 
     public static Vector<Tuple> BFS (boolean [][][] cheese, Tuple origin){
-	return new Vector<Tuple>();
+        checked = new HashSet<Tuple>();
+        q = new LinkedList<Vector<Tuple>>();
+        Vector<Tuple> originPath = new Vector<Tuple>();
+        originPath.add(origin);
+        q.add(originPath);
+        checked.add(origin);
+        while (!q.isEmpty()) {
+            Vector<Tuple> currentPath = q.remove();
+            Tuple currentNode = currentPath.lastElement();
+            if (currentNode.one==0) return currentPath;
+            check(cheese, currentNode, currentPath);
+        }
+    return new Vector<Tuple>();
     }
 
     public static Vector<Tuple> DFS (boolean [][][] cheese, Tuple origin){
-	return new Vector<Tuple>();
+        checked = new HashSet<Tuple>();
+        q = new LinkedList<Vector<Tuple>>();
+        Vector<Tuple> originPath = new Vector<Tuple>();
+        originPath.add(origin);
+        q.add(originPath);
+        checked.add(origin);
+        while (!q.isEmpty()) {
+            Vector<Tuple> currentPath = q.removeLast(); //der einzige unterschied zu BFS;
+            Tuple currentNode = currentPath.lastElement();
+            if (currentNode.one==0) return currentPath;
+            check(cheese, currentNode, currentPath);
+        }
+    return new Vector<Tuple>();
     }
 
 }
--- Emmentaler/app/Tuple.java
+++ Emmentaler/app/Tuple.java
@@ -1,14 +1,37 @@
 package app;
+import java.util.HashSet;
 
 // just because java sucks
 class Tuple {
+    private static int hashInfo = 40; // Trägt man hier mindestens die Seitenlänge des Käses ein gibt es keine Kollisionen!
 
-    int zero, one, two;
-
+    public static void setHashInfo(int h) {
+        hashInfo = h;
+    }
+    final int zero, one, two;
     Tuple (int zero, int one, int two){
 
         this.zero = zero;
         this.one  = one;
         this.two  = two;
+    }
+
+    public static void main(String... args) {
+        HashSet<Tuple> eins = new HashSet<Tuple>();
+        eins.add(new Tuple(1,1,1));
+        System.out.println(eins.contains(new Tuple(1,1,1))); // FALSE WENN MAN HASHCODE NICHT ÜBERSCHREIBT.
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Tuple)) return false;
+        if (obj==this) return true;
+        Tuple other=(Tuple)obj;
+        return (this.zero==other.zero&&this.one==other.one&&this.two==other.two);
+    }
+
+    @Override // Ich habe Angefangen mit HashSet, und bringe es jetzt zu Ende
+    public int hashCode() {
+        return (hashInfo*hashInfo*zero)+(hashInfo*one)+two; // Zu unserer Verteidung: Wenn hashInfo richtig gesetzt ist, ist das garantiert kollisionsfrei.
     }
 }
